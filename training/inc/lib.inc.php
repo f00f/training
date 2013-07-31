35b4ac356a0a81880d7d54d5f0d68300d22c9970
f00f <f00f@f00f.de>
2013-07-31 09:51:34 +0200

    Added pool images. Thanks for the idea, @pedak

--- training/inc/lib.inc.php
+++ training/inc/lib.inc.php
@@ -195,6 +195,17 @@ function UpdateFiles() {
 	$fh = fopen('inc/stats.html', 'w');
 	fwrite($fh, $html);
 	fclose($fh);
+	
+	// find random pool image
+	$fh = fopen('inc/bad.html', 'w');
+	$poolImageFolder = './badbilder/'.strtolower($nextTraining['ort']).'/';
+	if (!file_exists($poolImageFolder) || !is_dir($poolImageFolder)) {
+		fwrite($fh, '');
+	} else {
+		$badBildFile = RandomFile($poolImageFolder, 'jpg|png|gif');
+		fwrite($fh, '<a href="'.$badBildFile.'"><img id="badbild" src="'.$badBildFile.'" /></a>');
+	}
+	fclose($fh);
 
 	// store people's status
 	$html = '<strong class="zusage">zugesagt '.(1 == $anzahlZugesagt ? 'hat' : 'haben').' '.$anzahlZugesagt.':</strong><div id="zusager"><span>
@@ -213,6 +224,58 @@ function UpdateFiles() {
 	$fh = fopen('inc/beteiligung.html', 'w');
 	fwrite($fh, $html);
 	fclose($fh);
+}
+
+// from: http://www.jonasjohn.de/snippets/php/random-file.htm
+// mod: /i modifier for extensions pattern
+
+
+function RandomFile($folder='', $extensions='.*'){
+    // fix path:
+    $folder = trim($folder);
+    $folder = ($folder == '') ? './' : $folder;
+
+    // check folder:
+    if (!is_dir($folder)) { die('invalid folder given!'); }
+
+    // create files array
+    $files = array();
+
+    // open directory
+    if ($dir = @opendir($folder)) {
+        // go trough all files:
+        while ($file = readdir($dir)) {
+            if (!preg_match('/^\.+$/', $file) and
+                preg_match('/\.('.$extensions.')$/i', $file)) {
+
+                // feed the array:
+                $files[] = $file;
+            }
+        }
+        // close directory
+        closedir($dir);    
+    }
+    else {
+        die('Could not open the folder "'.$folder.'"');
+    }
+
+    if (count($files) == 0) {
+        die('No files where found :-(');
+    }
+
+    // seed random function:
+    mt_srand((double)microtime()*1000000);
+
+    // get an random index:
+    $rand = mt_rand(0, count($files)-1);
+
+    // check again:
+    if (!isset($files[$rand])) {
+        die('Array index was not found! very strange!');
+    }
+
+    // return the random file:
+    return $folder . $files[$rand];
 }
 
 function SendMail($p_action, $p_name, $p_anzZu, $p_anzAb, $p_next) {
